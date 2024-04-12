47efef5f6113e1609466d9fd2818f37ca4512dfc
Kimshii <117647269+Kimshii@users.noreply.github.com>
2024-04-12 16:31:00 +0200

    it works

--- app/database/rooms.py
+++ app/database/rooms.py
@@ -38,20 +38,18 @@ def show_room_reservations(room_name: str):
     filter = f"room_name = {room_name}"
     room_record = client.collection('grouprooms').get_list(1, 1, {'filter': filter})
     room = room_record.items[0].id
-    new_filter = f"room={room}"
+    new_filter = f"room.room_name={room_name}"
     #reservations_record = client.collection('reservations').get_list(1, 1, {filter: cool})
     try:
-       # aspdl =  client.collection('reservations').get_list(1, 1, {'filter': room})
-        #resultList =  client.records.getList('reservations', 1, 50, {'filter': new_filter})
-        1+1
+        reservation_record =  client.collection('reservations').get_list(1, 1, {'filter': new_filter})
     except:
         return "Very bad."
 
 
-    #reserved_times = {}
-    #for res in reservations_record.items:
-    #    if(res.room == room):
-    #       reserved_times.update({"start-time": res.starttime})
-    #        reserved_times.update({"end-time": res.endtime})
+    reserved_times = {}
+    for res in reservation_record.items:
+        if(res.room == room):
+            reserved_times.update({"start-time": res.starttime})
+            reserved_times.update({"end-time": res.endtime})
 
-    return room_record.items[0]
+    return reserved_times
