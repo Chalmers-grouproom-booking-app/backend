846c812b693125ca3e2bacd0ec021b31b1dee7f5
Melker Gustafsson <smelker02@gmail.com>
2024-04-12 14:50:46 +0200

    updated format fro result in search
    Co-authored-by: IsmailR16 <IsmailR16@users.noreply.github.com>

--- app/database/rooms.py
+++ app/database/rooms.py
@@ -2,32 +2,27 @@ from database.pb import client
 import json
 
 def get_room_info(room_name):
-    try:
-        thisfilter = f"room_name='{room_name}'"
-        room_record = client.collection('grouprooms').get_list(1, 1,  {'filter': thisfilter})
-        room_info = {
-                "Room Name": room_name,
-                "Room Size": f"{get_room_size(room_record)} square meters",
-                "Building": get_building(room_record),
-                "Campus": get_campus(room_record),
-                "Equipment": get_equipment(room_record)
-            }
-        if not room_info["Equipment"]:
+    # try:
+    thisfilter = f"room_name='{room_name}'"
+    room_record = client.collection('grouprooms').get_list(1, 1,  {'filter': thisfilter})
+    return room_format(room_record)
+    
+def room_format(room_record):
+    rooms = []
+    for item in room_record.items:
+        
+        room = {
+            "Room Name": item.room_name,
+            "Room Size": item.room_size,
+            "Building": item.building,
+            "Campus": item.campus,
+            "Equipment": item.equipment
+        }
+        if not room["Equipment"]:
             equipment = "There is no Equipments"
         else:
-            equipment = room_info["Equipment"]
+            equipment = room["Equipment"]
             
-        return {"Room Name": room_name,"Room Size":f"{get_room_size(room_record)} square meters","Building": get_building(room_record),"Campus": get_campus(room_record),"Equipment": equipment}
-    except:
-        return "This Room Does not exist"
-def get_room_size(room_record):
-    return room_record.items[0].room_size
-
-def get_building(room_record):
-    return room_record.items[0].building
-
-def get_campus(room_record):
-    return room_record.items[0].campus
-
-def get_equipment(room_record):
-    return room_record.items[0].equipment
+        rooms.append({"Room Name": item.room_name,"Room Size":f"{item.room_size} square meters","Building": item.building,"Campus": item.campus,"Equipment": equipment})
+    return rooms
+    
