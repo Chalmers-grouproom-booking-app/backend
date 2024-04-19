e0f9d2e09e02714244f015c82c81e35979ece77f
oKenzz <ken98ph@gmail.com>
2024-04-19 14:16:35 +0200

    Co-authored-by: Melker Gustafsson <McQueenMG@users.noreply.github.com> Co-authored-by: IsmailR16 <IsmailR16@users.noreply.github.com>

--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,10 @@
 import re
 from fastapi import HTTPException, status
 
-def validate_input(room_name: str) -> str:
-    if not re.match(r"^[a-zA-Z0-9\s_-]*$", room_name):
+def validate_input(input: str) -> str:
+    if not re.match(r"^[a-zA-Z0-9\s_-]*$", input):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid input, try again >:("
         )
-    return room_name
+    return input
