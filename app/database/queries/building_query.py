85934a81c1975f4e7339d05ebe04560797c3a97b
JacobAndersso <117654895+JacobAndersso@users.noreply.github.com>
2024-05-10 13:09:21 +0200

    started on get all building percentages same time
    Co-Authored-By: Kimshii <117647269+Kimshii@users.noreply.github.com>
    Co-Authored-By: p0ntu2 <117637978+p0ntu2@users.noreply.github.com>

--- app/database/queries/building_query.py
+++ app/database/queries/building_query.py
@@ -1,5 +1,6 @@
 from exceptions.exceptions import RoomNotFoundException
 from database.pb import client
+import pandas as pd
 
 class BuildingQuery:
     MAX_RESERVATIONS = 50
@@ -9,8 +10,18 @@ class BuildingQuery:
         
     def get_all_rooms_in_building(self):
         """Return all rooms in a building"""
-        reservation_filter = f"building='{self.building}'"
-        rooms = client.collection('grouprooms').get_list(1, self.MAX_RESERVATIONS, {'filter': reservation_filter}).items
+        building_filter = f"building='{self.building}'"
+        rooms = client.collection('grouprooms').get_list(1, self.MAX_RESERVATIONS, {'filter': building_filter}).items
         if not rooms:
             raise RoomNotFoundException(f"No building found called '{self.building}'")
         return rooms
+
+    def get_all_buildings(self):
+        """Return all buildings"""
+        data = client.collection("grouprooms").get_full_list()
+        print(pd.DataFrame(data))
+        buildings = []
+        for entry in data:
+            if (entry.building not in buildings):
+                buildings.append(entry.building)
+        return buildings
